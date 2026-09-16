995684c3bf8b9376235fe56707a55796ac7bf607
jvke <shubhambrown1@gmail.com>
2026-09-16 09:17:12 +0530

    HollowSquare problem

---
+++ patterns/HollowSquare.java
@@ -0,0 +1,25 @@
+// Pattern 2: Hollow Square Pattern
+
+// * * * * *
+// *       *
+// *       *
+// *       *
+// * * * * *
+
+class HollowSquare {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                if (row == 1 || row == n || col == 1 || col == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
