4adb8537eeb5b55c012e6de46678686cb38bef8a
jvke <shubhambrown1@gmail.com>
2026-09-16 13:39:44 +0530

    Solid Pyramid Pattern

---
+++ patterns/SolidPyramid.java
@@ -0,0 +1,22 @@
+// Pattern 5: Solid Pyramid Pattern
+
+//       *
+//     * * *
+//   * * * * *
+// * * * * * * *
+
+public class SolidPyramid {
+    public static void main(String[] args) {
+        int n = 4;
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n - row; col++) {
+                System.out.print("  ");
+            }
+            for (int col2 = 1; col2 <= row + (row - 1); col2++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
