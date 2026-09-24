e80bce7c78e3677e3ce1c2cbf51142ff74f2e890
jvke <shubhambrown1@gmail.com>
2026-09-24 18:52:13 +0530

    Rhombus Pattern

---
+++ patterns/Rhombus.java
@@ -0,0 +1,25 @@
+// Rhombus Pattern
+
+//         * * * * *
+//       * * * * *
+//     * * * * *
+//   * * * * *
+// * * * * *
+
+public class Rhombus {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n - row; col++) {
+                System.out.print("  ");
+            }
+
+            for (int col2 = 1; col2 <= n; col2++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
